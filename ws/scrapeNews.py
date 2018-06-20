51d364e902d7061311b4f0f75303364977d0af7c
Rauha <yunlong.a.liu@gmail.com>
2018-06-20 21:25:22 +0300

    Remove [' '] of article content and titles make it more readable.

--- ws/scrapeNews.py
+++ ws/scrapeNews.py
@@ -9,7 +9,15 @@ from ws.models import News
 import datefinder
 import datetime
 
-# get urls of MostRead
+
+"""
+************Saving to database******************
+date format :YYYY-MM-DD and it should be string
+content should be string
+title should be string 
+************************************************
+
+"""
 
 name_bbc = "bbc"
 name_reuters = "reuters"
@@ -29,12 +37,11 @@ def latest_business_news_bbc():
 def article_content__bbc(url):
     resp = requests.get(url)
     sop = bs4.BeautifulSoup(resp.text, 'html.parser')
-    title = []
     content = []
     article_date_tem = sop.find('div', class_='date').text
     # convert date format into YYYY-MM-DD
     article_date = time.strftime('%Y-%m-%d', time.localtime(time.mktime(time.strptime(article_date_tem, '%d %B %Y'))))
-    title.append(sop.find('h1').text)
+    title = str(sop.find('h1').text)
     p = sop.find('div', property='articleBody')
     for i in p.find_all('p'):
         content.append(str(i))
@@ -61,8 +68,9 @@ def article_content_reuters(url):
     # find date in text, it can output date in format
     matches = datefinder.find_dates(soup.find('div', class_='date_V9eGk').text)
     for match in matches:
+        # convert date format into YYYY-MM-DD
         date = str(datetime.datetime.date(match))
-    titles.append(soup.find('div', class_='content-container_3Ma9y').h1.text)
+    titles = str(soup.find('div', class_='content-container_3Ma9y').h1.text)
     content = soup.find('div', class_='body_1gnLA')
     for i in content.find_all('p'):
         contents.append(str(i))
@@ -85,16 +93,16 @@ def latest_business_news_ibt():
 
 def article_content_ibt(url):
     contents = []
-    titles = []
     resp = requests.get(url)
     soup = bs4.BeautifulSoup(resp.text, 'html.parser')
     date_ibt = datefinder.find_dates(soup.find('time', itemprop='datePublished').text)
     for i in date_ibt:
+        # convert date format into YYYY-MM-DD
         date = str(datetime.datetime.date(i))
     soup_1 = soup.find('div', class_='article-body')
     for content in soup_1.find_all('p'):
         contents.append(str(content))
-    titles.append(soup.find('header', class_='article-header').h1.text)
+    titles = str(soup.find('header', class_='article-header').h1.text)
     return titles, date, contents
 
 
@@ -103,19 +111,23 @@ def save_article_content():
     link_reuters = latest_business_news_reuters()
     link_ibt = latest_business_news_ibt()
     for i in link_bbc:
-        title, article_date, content = article_content__bbc(i)
-        News.objects.get_or_create(name=name_bbc, title=title,  date=article_date, content=content)
+        title, article_date, content_bbc = article_content__bbc(i)
+        #convert list to string
+        contents_bbc = ''.join(str(e) for e in content_bbc)
+        News.objects.get_or_create(name=name_bbc, title=title,  date=article_date, content=contents_bbc)
     print("***************BBC Done****************")
 
     for i in link_reuters:
-        title, article_date, content = article_content_reuters(i)
-        News.objects.get_or_create(name=name_reuters, title=title, date=article_date, content=content)
+        title, article_date, content_reuters = article_content_reuters(i)
+        contents_reuters = ''.join(str(e) for e in content_reuters)
+        News.objects.get_or_create(name=name_reuters, title=title, date=article_date, content=contents_reuters)
     print("***************REUTERS Done****************")
 
     for i in link_ibt:
-        title, article_date, content = article_content_ibt(i)
-        News.objects.get_or_create(name=name_ibt, title=title, date=article_date, content=content)
-# get_or_create() can ensure data is unique
+        title, article_date, content_ibt = article_content_ibt(i)
+        contents_ibt = ''.join(str(e) for e in content_ibt)
+        News.objects.get_or_create(name=name_ibt, title=title, date=article_date, content=contents_ibt)
+        # get_or_create() can ensure data is unique
     print("***************IBT Done****************")
 
 
